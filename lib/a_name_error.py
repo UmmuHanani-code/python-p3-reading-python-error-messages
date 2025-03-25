98b691f82cd1c00d17c4a8d5fb12967ada2b60a4
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-03-25 13:26:36 +0300

    commit types of excetion Errors

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
-
+hello_world = "This is testing NameError"
 print(hello_world)
--- lib/a_type_error.py
+++ lib/a_type_error.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
-wrong_type = 'abc' + 123
+#wrong_type = 'abc' + 123
+wrong_type = 1 + 2;
+print("This is correcting {wrong_type} TypeError")
